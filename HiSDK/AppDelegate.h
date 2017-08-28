1d6feae95296843692af163cefa59a9eefcffdea
TuanAnh <anhvuvantuan@vccorp.com>
2017-08-28 08:53:34 +0700

    Initial Commit

---
+++ HiSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HiSDK
+//
+//  Created by TuanAnh on 8/28/17.
+//  Copyright © 2017 TuanAnh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HiSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HiSDK
+//
+//  Created by TuanAnh on 8/28/17.
+//  Copyright © 2017 TuanAnh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
